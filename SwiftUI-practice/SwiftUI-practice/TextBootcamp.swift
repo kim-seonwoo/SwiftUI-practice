8a47284b18002ec4c8cf3696de3790685b7c0025
kim-seonwoo <sw204011@gmail.com>
2024-02-06 21:41:29 +0900

    class #2

---
+++ SwiftUI-practice/SwiftUI-practice/TextBootcamp.swift
@@ -0,0 +1,23 @@
+//
+//  TextBootcamp.swift
+//  SwiftUI-practice
+//
+//  Created by Seonwoo Kim on 2/6/24.
+//
+
+import SwiftUI
+
+struct TextBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//            .font(.title)
+//            .fontWeight(.semibold)
+//            .italic()
+//            .strikethrough(true, color: .red)
+            .font(.system(size: 24, weight: .semibold, design: .serif))
+    }
+}
+
+#Preview {
+    TextBootcamp()
+}
