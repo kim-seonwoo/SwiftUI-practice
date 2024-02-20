e90b53d91efbe25358b46b8d254118798a92cfe3
kim-seonwoo <sw204011@gmail.com>
2024-02-20 16:46:18 +0900

    SwiftUI Tutorials #2

---
+++ SwiftUI-practice/SwiftUI-practice/Landmark/Models/Landmark.swift
@@ -0,0 +1,41 @@
+//
+//  Landmark.swift
+//  SwiftUI-practice
+//
+//  Created by Seonwoo Kim on 2/20/24.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+
+
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
+
+// You make the property private because users of the Landmarks structure care only about the image itself.
