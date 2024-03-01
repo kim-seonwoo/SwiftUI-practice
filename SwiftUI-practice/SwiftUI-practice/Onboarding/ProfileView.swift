25254829be81fb7ecf2efada4e32fc0ce1bcee57
kim-seonwoo <sw204011@gmail.com>
2024-03-01 23:48:58 +0900

    add kakao

--- SwiftUI-practice/SwiftUI-practice/Onboarding/ProfileView.swift
+++ SwiftUI-practice/SwiftUI-practice/Onboarding/ProfileView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import KakaoSDKUser
 
 struct ProfileView: View {
     
@@ -21,6 +22,36 @@ struct ProfileView: View {
                 .scaledToFit()
                 .frame(width: 150, height: 150)
             Text(currentUserName ?? "Your name here")
+            Button {
+                if (UserApi.isKakaoTalkLoginAvailable()) {
+                    UserApi.shared.me { User, Error in
+                        if let name = User?.kakaoAccount?.profile?.nickname {
+                            print(name)
+                        }
+                        if let mail = User?.kakaoAccount?.email {
+                            print(mail)
+                        }
+                        if let profile = User?.kakaoAccount?.profile?.profileImageUrl {
+                            print(profile)
+                        }
+                    }
+                    UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                        print(oauthToken)
+                        print(error)
+                    }
+                } else {
+                    UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                        print(oauthToken)
+                        print(error)
+                    }
+                }
+            } label : {
+                Image("dragon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width : UIScreen.main.bounds.width * 0.9)
+            }
+            
             Text("This user is \(currentUserAge ?? 0) years old")
             Text("Their gender is \(currentUserGender ?? "unknown")")
             Text("Sign Out")
@@ -33,18 +64,18 @@ struct ProfileView: View {
                 .onTapGesture {
                     signOut()
                 }
-//            Text("Main")
-//                .foregroundColor(.white)
-//                .frame(height: 55)
-//                .frame(maxWidth: .infinity)
-//                .background(.black)
-//                .font(.headline)
-//                .cornerRadius(10)
-//                .onTapGesture {
-//                    print(path)
-//                    pushToMain()
-//                    print(path)
-//                }
+            //            Text("Main")
+            //                .foregroundColor(.white)
+            //                .frame(height: 55)
+            //                .frame(maxWidth: .infinity)
+            //                .background(.black)
+            //                .font(.headline)
+            //                .cornerRadius(10)
+            //                .onTapGesture {
+            //                    print(path)
+            //                    pushToMain()
+            //                    print(path)
+            //                }
             
             NavigationLink(destination: ListView()) {
                 Text("Main")
@@ -64,7 +95,7 @@ struct ProfileView: View {
         .shadow(radius: 10)
         .padding(40)
     }
-        
+    
     
     func signOut() {
         currentUserName = nil
--- SwiftUI-practice/SwiftUI-practice/SwiftUI_practiceApp.swift
+++ SwiftUI-practice/SwiftUI-practice/SwiftUI_practiceApp.swift
@@ -5,16 +5,27 @@
 //  Created by Seonwoo Kim on 2/6/24.
 //
 
+
+
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct SwiftUI_practiceApp: App {
+    init() {
+        // Kakao SDK 초기화
+        KakaoSDK.initSDK(appKey:"c7d3f8f7ad71395d5532803926da17ce")
+    }
+    
     var body: some Scene {
         WindowGroup {
-            OnboardingSwipeView()
+            ContentView()
+                .onOpenURL { url in
+                    if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                        _ = AuthController.handleOpenUrl(url: url)
+                    }
+                }
         }
     }
 }
-
-
-
