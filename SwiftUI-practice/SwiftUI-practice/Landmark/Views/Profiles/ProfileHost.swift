f73476e0b0f0821749c3e9a87af0169d2932f5e0
kim-seonwoo <sw204011@gmail.com>
2024-03-06 20:21:38 +0900

    swift tutorial profile part add

---
+++ SwiftUI-practice/SwiftUI-practice/Landmark/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,48 @@
+//
+//  ProfileHost.swift
+//  SwiftUI-practice
+//
+//  Created by Seonwoo Kim on 3/6/24.
+//
+import SwiftUI
+
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    @Environment(\.editMode) var editMode
+    @Environment(ModelData.self) var modelData
+
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                if editMode?.wrappedValue == .active {
+                    Button("Cancel", role: .cancel) {
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
+                Spacer()
+                EditButton()
+            }
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        modelData.profile = draftProfile
+                    }
+            }
+        }
+        .padding()
+    }
+}
+
+
+#Preview {
+    ProfileHost()
+        .environment(ModelData())
+}
